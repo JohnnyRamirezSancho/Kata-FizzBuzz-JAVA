5f648bec17722c94c1a9bf3dcc9821f5e2e2f9ed
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-01-09 11:47:45 +0100

    Test fizz and tezt buzz

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,42 @@
 package com.fizzbuzz;
 
 public class FizzBuzz {
-    
+    private Integer number;
+
+    public FizzBuzz(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Secuencia de numeros");
+        System.out.println("Del 1 al 100");
+        System.out.print("Serie: ");
+
+        for (num = 1; num <= 100; num++) {
+            System.out.print(num + ", ");
+        }
+    }
+
+    public String fizz () {
+        if(this.number % 3 == 0){
+            return "fizz";
+        }
+        return String.valueOf(this.number);
+    }
+
+    public String buzz () {
+        if(this.number % 5 == 0){
+            return "buzz";
+        }
+        return String.valueOf(this.number);
+    }
 }
--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -1,8 +1,8 @@
 package com.fizzbuzz;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
     
@@ -11,4 +11,22 @@ public class FizzBuzzTest {
         assertEquals(2, 2);
     }
 
+    @Test
+    public void test_fizzbuzz_assign_value_number() {
+        FizzBuzz mifizzBuzz = new FizzBuzz(5);
+        mifizzBuzz.setNumber(6);
+        assertEquals(6, mifizzBuzz.getNumber());
+    }
+
+    @Test
+    public void test_fizzbuzz_return_fizz_divisible_3() {
+        FizzBuzz mifizzBuzz = new FizzBuzz(9);
+        assertEquals("fizz", mifizzBuzz.fizz());
+    }
+
+    @Test
+    public void test_fizzbuzz_return_buzz_divisible_5() {
+        FizzBuzz mifizzBuzz = new FizzBuzz(5);
+        assertEquals("buzz", mifizzBuzz.buzz());
+    }
 }
